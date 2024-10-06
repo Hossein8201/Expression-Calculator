552720b785d45ce3822e22fb594cc3c2ce13ac84
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-10-06 21:36:29 +0330

    feat: the undefined operation condition

--- calculator.java
+++ calculator.java
@@ -1,15 +1,15 @@
+// This is the JAVA code for Calculator Program:
 import java.util.Scanner;
 
 public class calculator {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-
+        // The inputs variables and their data types:
         double firstNumber = scanner.nextDouble();
         String operation = scanner.next();
         double secondNumber = scanner.nextDouble();
-
         double resault = 0;
-
+        // Define the operation that we want to do:
         switch (operation) {
             case "+":
                 resault = firstNumber + secondNumber;
@@ -23,10 +23,12 @@ public class calculator {
             case "/":
                 resault = firstNumber / secondNumber;
                 break;
+            // the condition that show us the operation is not define for program:
             default:
-                break;
+                System.out.println("the operation is not defined!!!");
+                return;
         }
-        
-        System.out.println("the resault is : " + firstNumber+" "+operation+" "+secondNumber+" = "+resault);
+        // show the answer:
+        System.out.println("the resault is : " +firstNumber+" "+operation+" "+secondNumber+" = "+resault);
     }
 }
